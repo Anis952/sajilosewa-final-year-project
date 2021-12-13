a65e41d3a270216d943ac859f32ab2d176090904
Anish <thapaanes03@gmail.com>
2021-12-13 20:19:25 +0545

    Added first model Customer

--- delivery/settings.py
+++ delivery/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mainapp.middleware.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'delivery.urls'
