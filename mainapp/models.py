a4f0d6c966cd9db5dbf671a4fab49caa2bf5d89c
Anish Thapa <thapaanes03@gmail.com>
2022-01-29 13:04:04 +0545

    Created the templates to add job or product

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
+from django.utils import timezone
 
 # Create your models here.
 
@@ -15,3 +17,4 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()  
+
